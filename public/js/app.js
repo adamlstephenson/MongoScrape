80fab56bd08151388302b620dc15d25b8c7902d8
Adam Stephenson <astephensonx@gmail.com>
2018-06-25 21:02:22 -0500

    heroku setup

--- public/js/app.js
+++ public/js/app.js
@@ -10,13 +10,14 @@ $.getJSON("/articles", function(data) {
     for (var i = 0; i < data.length; i++) {
         let button = $("<button>")
       // Display the apropos information on the page
-      $("#articles").append("<a href='" + data[i].link + "' 'data-id='" + data[i]._id + "'>" + data[i].headline + "<button 'class=button-success' data-id=" + data[i]._id + "> Save </button>" + "</a>");
-      $("p").addClass("article-divs");
+      $("#articles").append("<a href='" + data[i].link + "' 'data-id='" + data[i]._id + "'>" + data[i].headline + "<button 'class=button-success' data-id=" + data[i]._id + "> Save </button>" + "<br />" + "</a>");
+      $("a").addClass("article-divs");
+      $("button").attr("id", "save-button")
     //   $(".article-divs").append(button);
     }
   });
 
-  $(document).on("click", "p", function () {
+  $(document).on("click", ".article-divs", function () {
 
       $("#notes").empty();
 
--- server.js
+++ server.js
@@ -19,17 +19,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 // Set Handlebars.
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-//Connect to Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/scraper")
+// //Connect to Mongo DB
+// mongoose.connect("mongodb://localhost/scraper")
 
 //Routes
 
