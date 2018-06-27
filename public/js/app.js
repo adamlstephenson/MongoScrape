78f83796b97bf524e9a404dcb607b7f0e8715a8a
Adam Stephenson <astephensonx@gmail.com>
2018-06-26 21:38:08 -0500

    more styling and trying to fix notes

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,29 @@
-//Get articles and display on page
-// $.getJSON("/articles", (data) => {
-//     data.forEach((() => {
-//         $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].headline + "<br />" + data[i].link + "</p>")
-//     }))
-// })
-
-$.getJSON("/articles", function(data) {
+$.getJSON("/articles", function (data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
         let button = $("<button>")
-      // Display the apropos information on the page
-      $("#articles").append("<a class='article-divs' href='" + data[i].link + "' 'data-id='" + data[i]._id + "'>" + data[i].headline + "<button class='button-success' data-id=" + data[i]._id + "> Save </button>" + "<br />" + "</a>");
+        let div = $("<div class='col-md-12' id='article-divs'>")
+        // Display the apropos information on the page
+        $("#articles").append(div);
+        $("#article-divs").append("<a class='article-divs' href='" + data[i].link + "' data-id='" + data[i]._id + "'>" + data[i].headline + "<button class='button-success' data-id=" + data[i]._id + "> Save </button>" + "<br />" + "</a>");
+        $("#article-divs").append("<p class='summary-text' data-id='" + data[i]._id + "'>Summary goes here (click to add note)</p>")
     }
-  });
 
-  $(document).on("click", ".article-divs", function () {
+    //$("#articles").append("<p class='summary-text'>Summary goes here</p>")
+});
 
-      $("#notes").empty();
+$(document).on("click", ".summary-text", function () {
 
-      let thisId = $(this).attr("data-id");
+    $("#notes").empty();
 
-      $.ajax({
-          method: "GET",
-          url: "/articles/" + thisId
-      })
-      .then(((data) => {
-          console.log(data)
-      }))
+    let thisId = $(this).attr("data-id");
 
-  })
-  
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })
+        .then(((data) => {
+            console.log(data)
+        }))
+
+})
